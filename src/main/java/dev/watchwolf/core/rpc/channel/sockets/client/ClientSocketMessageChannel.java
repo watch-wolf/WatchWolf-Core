945c6efeb6a34309496d6d6de5f8362f4bdf1049
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-12-19 22:15:22 +0100

    Fix: get/send big chunks of bytes

--- src/main/java/dev/watchwolf/core/rpc/channel/sockets/client/ClientSocketMessageChannel.java
+++ src/main/java/dev/watchwolf/core/rpc/channel/sockets/client/ClientSocketMessageChannel.java
@@ -8,9 +8,16 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
 public class ClientSocketMessageChannel extends SocketMessageChannel {
+    /**
+     * Max bytes that can be sent to a socket.
+     * For more information check <a href="https://www.ibm.com/docs/en/ztpf/2020?topic=apis-send-send-data-connected-socket">IBM - Send data on a connected socket</a>.
+     */
+    public static final int MAX_SOCKET_MESSAGE_LENGTH = 65536;
+
     private Socket socket;
 
     public ClientSocketMessageChannel(String host, int port) {
@@ -30,7 +37,7 @@ public class ClientSocketMessageChannel extends SocketMessageChannel {
     @Override
     public void send(byte[] data) throws IOException {
         DataOutputStream dos = new DataOutputStream(this.socket.getOutputStream());
-        dos.write(data);
+        for (byte []maxLenghtData : splitBytes(data, MAX_SOCKET_MESSAGE_LENGTH)) dos.write(maxLenghtData);
     }
 
     @Override
@@ -42,12 +49,37 @@ public class ClientSocketMessageChannel extends SocketMessageChannel {
 
         try {
             byte []r = new byte[numBytes];
-            int outBytes = dis.read(r);
-            if (outBytes != numBytes) throw new IOException("Couldn't read " + numBytes + " bytes (read " + outBytes + " instead)");
+            segmentedRead(dis, r);
             return r;
         } catch (SocketTimeoutException ex) {
             throw new TimeoutException(ex.getMessage());
         }
+    }
+
+    private int segmentedRead(DataInputStream dis, byte []r) throws IOException,SocketTimeoutException {
+        int outBytes = 0, tmp;
+        int numBytes = r.length;
+
+        if (numBytes <= MAX_SOCKET_MESSAGE_LENGTH) {
+            // we can read it in one try
+            outBytes = dis.read(r);
+            if (outBytes != numBytes) throw new IOException("Couldn't read " + numBytes + " bytes (read " + outBytes + " instead)");
+        }
+        else {
+            // too big; we need to split it
+            while (numBytes > 0) {
+                byte []chunk = new byte[Math.min(numBytes, MAX_SOCKET_MESSAGE_LENGTH)];
+                tmp = dis.read(chunk);
+                if (tmp != chunk.length) throw new IOException("Couldn't read chunk of " + chunk.length + " bytes (read " + tmp + " instead)");
+
+                System.arraycopy(chunk, 0, r, outBytes, chunk.length);
+
+                outBytes += tmp;
+                numBytes -= MAX_SOCKET_MESSAGE_LENGTH; // it doesn't matter if `numBytes` is negative; we only check if there's something left to read
+            }
+        }
+
+        return outBytes;
     }
 
     @Override
@@ -63,5 +95,30 @@ public class ClientSocketMessageChannel extends SocketMessageChannel {
     @Override
     public void close() throws IOException {
         this.socket.close();
+    }
+
+    /**
+     * Splits an array into smaller arrays of a fixed size
+     * @author https://stackoverflow.com/a/32179121/9178470
+     *
+     * @param data Array to split
+     * @param chunkSize Max size of a sub-array
+     * @return Array of chunk-sized arrays
+     */
+    public static byte[][] splitBytes(final byte[] data, final int chunkSize)
+    {
+        final int length = data.length;
+        final byte[][] dest = new byte[(length + chunkSize - 1)/chunkSize][];
+        int destIndex = 0;
+        int stopIndex = 0;
+
+        for (int startIndex = 0; startIndex + chunkSize <= length; startIndex += chunkSize) {
+            stopIndex += chunkSize;
+            dest[destIndex++] = Arrays.copyOfRange(data, startIndex, stopIndex);
+        }
+
+        if (stopIndex < length) dest[destIndex] = Arrays.copyOfRange(data, stopIndex, length);
+
+        return dest;
     }
 }
--- src/test/java/dev/watchwolf/core/rpc/channel/sockets/SocketMessageChannelShould.java
+++ src/test/java/dev/watchwolf/core/rpc/channel/sockets/SocketMessageChannelShould.java
@@ -39,4 +39,35 @@ public class SocketMessageChannelShould {
             if (client != null) client.close();
         }
     }
+
+    @Test
+    public void sendHugeData() throws Exception {
+        int numBytes = 65536*4; // 4 times more than the limit
+
+        String host = "127.0.0.1";
+        int port = 8900;
+        MessageChannel server = null, client = null;
+
+        byte[] toSend = new byte[numBytes];
+        for (int i = 0; i < numBytes; i++) toSend[i] = (byte) (i % 128);
+
+        try {
+            server = new ServerSocketChannelFactory(host, port).build().create();
+            client = new ClientSocketChannelFactory(host, port).build().create();
+
+            ((ServerSocketMessageChannel)server).acceptConnection(); // allow client to connect
+
+            client.send(toSend);
+
+            byte[] got = server.get(toSend.length, 5000);
+            assertEquals(toSend.length, got.length, "Different length got");
+
+            assertTrue(Arrays.equals(toSend, got), "Got different between sent and got. Sent: " + Arrays.toString(toSend) + "; got: " + Arrays.toString(got));
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            if (server != null) server.close();
+            if (client != null) client.close();
+        }
+    }
 }
