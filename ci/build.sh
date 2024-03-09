5ab4f4f39b3d8fd12ff31d4a59e746aad4c4bc4c
rogermiranda1000 <rogermirandaperez@hotmail.com>
2024-03-09 22:52:17 +0100

    CI changes

--- ci/build.sh
+++ ci/build.sh
@@ -2,13 +2,11 @@
 
 # default variables
 preclean=0
-skip_tests="true"
 
 # parse params
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --preclean) preclean=1 ;;
-        --include-tests) skip_tests="false" ;;
         
         *) echo "[e] Unknown parameter passed: $1" >&2 ; exit 1 ;;
     esac
@@ -23,7 +21,7 @@ local_maven_repos_path="$HOME/.m2"
 if [ $preclean -eq 1 ]; then
     docker run -it --rm -v "$base_path":"/compile" -v "$local_maven_repos_path":/root/.m2 maven:3.8.4-openjdk-8 mvn clean --file '/compile' # clean project & launch "clean" phase (if any)
 fi
-docker run -it --rm -v "$base_path":"/compile" -v "$local_maven_repos_path":/root/.m2 maven:3.8.4-openjdk-8 mvn compile assembly:single -DskipTests=true -Dmaven.test.skip="$skip_tests" --file '/compile'
+docker run -it --rm -v "$base_path":"/compile" -v "$local_maven_repos_path":/root/.m2 maven:3.8.4-openjdk-8 mvn compile assembly:single -Dmaven.test.skip=true --file '/compile'
 
 if [ $? -ne 0 ]; then
     echo "[e] Exception while compiling WW-Core"
